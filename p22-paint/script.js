76d7188ffcdba48953a75ed4dd64f909f5085e88
man9me <man9me2021@gmail.com>
2021-04-24 23:22:40 +0300

    p22-paint

--- p22-paint/script.js
+++ p22-paint/script.js
@@ -1,43 +1,85 @@
-const fill = document.querySelector('.fill')
-const empties = document.querySelectorAll('.empty')
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
 
-fill.addEventListener('dragstart', dragStart)
-fill.addEventListener('dragend', dragEnd)
+const ctx = canvas.getContext('2d');
 
+let size = 10
+let isPressed = false
+let color = 'black'
+let x
+let y
 
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
 
-for (const empty of empties) {
-    empty.addEventListener('dragover', dragOver)
-    empty.addEventListener('dragenter', dragEnter)
-    empty.addEventListener('dragleave', dragLeave)
-    empty.addEventListener('drop', dragDrop)
+    x = e.offsetX
+    y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
+})
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
 }
 
-function dragStart() {
-    this.className += ' hold'
-    setTimeout(() => this.className = 'invisible', 0)
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
 }
 
-function dragEnd() {
-    this.className = 'fill'
+function updateSizeOnScreen() {
+    sizeEL.innerText = size
 }
 
-function dragOver(e) {
-    e.preventDefault()
-}
+increaseBtn.addEventListener('click', () => {
+    size += 5
 
-function dragEnter(e) {
-    e.preventDefault()
+    if (size > 50) {
+        size = 50
+    }
 
-    this.className += " hovered"
-}
+    updateSizeOnScreen()
+})
 
-function dragLeave() {
-    this.className = "empty"
-}
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
 
-function dragDrop() {
-    this.className = 'empty'
+    if (size < 5) {
+        size = 5
+    }
 
-    this.append(fill)
-}
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
